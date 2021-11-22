600740f031eca76d521d0953f86a38c71a45c264
MoraaOnduso <mary.ondusoo@gmail.com>
2021-11-22 09:47:11 -0800

    replace

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-
-
 def new_in_list(my_list, idx, element):
-
-    if (idx < 0) or (idx > len(my_list)-1)):
-        return my_list
-
-    copy= [x for x in my_list]
-    copy[idx] = element
-    return copy
+    cp = []
+    for i in range(len(my_list)):
+        cp.append(my_list[i])
+    if idx < 0:
+        return cp
+    elif idx >= len(my_list):
+        return cp
+    else:
+        cp[idx] = element
+        return cp
