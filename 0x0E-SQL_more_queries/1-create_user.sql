9f3c72082670f9e53c547c74ef79d04b32152551
MoraaOnduso <mary.ondusoo@gmail.com>
2022-02-02 09:51:50 -0800

    uspr

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,3 @@
+-- Creates server user user_0d_1
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEDGES * . * TO 'user_0d_1'@'localhost';
---
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,2 @@
+-- creates table id_not_null
+CREATE IF NOT EXISTS id_not_null (id INT = 
