53991c0f7fec15e72da086d8e2ee710ec870c5c3
MoraaOnduso <mary.ondusoo@gmail.com>
2022-02-04 05:42:27 -0800

    nfull

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,5 +1,5 @@
 -- creates second_table and adds multiple rows
-CRETE TABLE IF NOT EXISTS second_table ('id' INT, 'name' VARCHAR(256), 'score' INT);
+CREATE TABLE IF NOT EXISTS second_table ('id' INT, 'name' VARCHAR(256), 'score' INT)
 INSERT INTO second_table VALUES 
        (1, "John", 10),
        (2, "Alex", 3),
