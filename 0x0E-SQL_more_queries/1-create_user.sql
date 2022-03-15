cd258a50aa1527892f95c9c778d34bb7dfe53ad2
MoraaOnduso <mary.ondusoo@gmail.com>
2022-03-15 04:19:55 -0700

    fxlst

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- Creates server user user_0d_1
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEDGES ON * . * TO 'user_0d_1'@'localhost';
-FLUSH PRIVILEDGES;
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+SET PASSWORD FOR 'user_0d_1'@'localhost' = 'user_0d_1_pwd';
+GRANT ALL PRIVILEDGES ON *.* TO 'user_0d_1'@'localhost';
--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,7 +1,3 @@
 -- creates table id_not_null
-CREATE IF NOT EXISTS id_not_null
-(
-	id INT DEFAULT 1,
-	name VARCHAR(256)
-);
+CREATE IF NOT EXISTS id_not_null(id INT DEFAULT 1, name VARCHAR(256));
 
