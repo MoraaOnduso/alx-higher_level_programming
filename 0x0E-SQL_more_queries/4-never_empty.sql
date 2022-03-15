d8645a43bdd1eb4bd4a8b8da4db62b5070eeb8ca
MoraaOnduso <mary.ondusoo@gmail.com>
2022-03-15 03:50:22 -0700

    tuklt

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,7 +1,7 @@
 -- creates table id_not_null
 CREATE IF NOT EXISTS id_not_null
 (
-	id INT NOT NULL DEFAULT 1,
+	id INT DEFAULT 1,
 	name VARCHAR(256)
 );
 
--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,1 +1,5 @@
 -- lists all cities in hbtn_0d_usa
+SELECT cities.id, cities.name, states.name
+FROM states
+INNER JOIN cities
+ON states.id = cities.state_id;
